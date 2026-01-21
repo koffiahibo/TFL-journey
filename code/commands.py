18364054054bdbc3378de4b94cf24b5f9589c110
koffiahibo <koffiahibo2024@gmail.com>
2026-01-21 14:41:15 +0000

    Api parameters empty space requests error

--- code/commands.py
+++ code/commands.py
@@ -8,15 +8,15 @@ close_alfc = "51.496212, 0.157618"
 mofde = "public-bus"
 
 def req_params():
-    sfrom = input("From: ")
-    to = input("To: ")
+    sfrom = "SW177TJ" #input("From: ")
+    to =  "SW81JJ" #input("To: ")
     via=input("~~Preferences~~\n"
           "Via: ")
     date = input("Date (yyyyMMdd format): ")
     time = input("Time (HHmm format): ")
     mode = input("Modes of travel (Eg: public-bus,overground,train,tube,coach,dlr,cablecar,tram,river,walking,cycle): ")
     preferences = input("Preferences(\"leastinterchange\" | \"leasttime\" | \"leastwalking\"): ")
-    parameters ={"via":via,
+    parameters0 ={"via":via,
                 "mode":mode,
                 "date":date,
                 "time":time,
@@ -24,6 +24,13 @@ def req_params():
                  "preferences":preferences,
                 "useRealTimeLiveArrivals" :True
                 }
+    parameters = {}
+    for keys, values in parameters0.items():
+        if isinstance(values, str):
+            values = values.strip()
+        if values != "" and values != None:
+            parameters[keys] = values
+
     return [sfrom, to, parameters]
 def plan_journey(sfrom, to, params):
     # via = None, mode = None, date = None, time = None, timeAD = None, live_time = None, journey_preferences = None)
@@ -39,6 +46,7 @@ def plan_journey(sfrom, to, params):
     # Implementation
     journey_url = f"https://api.tfl.gov.uk/Journey/JourneyResults/{sfrom}/to/{to}"
     response = requests.get(journey_url, params=params)
+    print(response.request.url)
     tex_req = json.loads(response.text)
     journeys=[]
     for journey in tex_req['journeys']:
@@ -46,7 +54,7 @@ def plan_journey(sfrom, to, params):
         end_time = journey['arrivalDateTime']
         duration = journey["duration"]
         try :
-            total_cost = journey["fare"]["totalCost"]
+            total_cost = journey["fare"]["totalCost"]/100
             peak_fare = journey["fare"]["fares"][0]["peak"]
         except KeyError :
             total_cost = "N/A"
@@ -60,7 +68,7 @@ def plan_journey(sfrom, to, params):
                     "Mode": leg["mode"]["id"] }
             legs.append(leg_dict)
         ##
-        journeys.append({"From": From, "To": To, "start time": start_time, "end time": end_time,
+        journeys.append({"From": sfrom, "To": to, "start time": start_time, "end time": end_time,
                          "duration": duration, "Total Cost": total_cost,
                          "Peak price": peak_fare, "legs":legs})
     return journeys # list of dictionnaries
@@ -75,5 +83,5 @@ def display_journeys(journey_data): # journey data is an element of the list ret
     )
     return
 sfrom, to, params = req_params()
-plan_journey(sfrom, to, params)
-display_journeys(plan_journey(sfrom, to, params)[0])
+for a in plan_journey(sfrom, to, params):
+    display_journeys(a)
